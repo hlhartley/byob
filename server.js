a92a439efb80dc95dfee66d231b8acfd3078eba6
Heather Hartley <hlhartley@gmail.com>
2019-03-20 13:48:40 -0600

    Adjusts order of lines

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-app.use(cors());
-app.use(express.json());
-
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.set('port', process.env.NODE_ENV || 3001)
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (request, response) => {
   response.send('welcome to states')
 });
